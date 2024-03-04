4fb716346822d9d50f1bd999be48482fbe4eba9e
ChatGTHB <cihat.kose@hotmail.com>
2024-03-04 21:13:04 +0100

    Edited

--- src/test/java/_13_Wait/Wait.java
+++ src/test/java/_13_Wait/Wait.java
@@ -3,6 +3,7 @@ package _13_Wait;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,6 +12,7 @@ import org.testng.annotations.Test;
 import utility.BaseDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 
 public class Wait extends BaseDriver {
@@ -19,18 +21,34 @@ public class Wait extends BaseDriver {
 
     @Test
     public void test1() {
+
         driver.get("http://seleniumpractise.blogspot.com/2016/08/how-to-use-explicit-wait-in-selenium.html");
 
-        WebElement button = driver.findElement(By.xpath("//*[text()='Click me to start timer']"));
-        button.click();
+        WebElement clickMe = driver.findElement(By.xpath("//*[text()='Click me to start timer']"));
+        clickMe.click();
 
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()='WebDriver']")));
-        Assert.assertTrue(element.getText().contains("WebDriver"));
+        // The first method
+        WebElement webDriverText = driver.findElement(By.xpath("//p[text()='WebDriver']"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()='WebDriver']")));
+
+        // The second method
+        // WebElement webDriverText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()='WebDriver']")));
+
+        Assert.assertTrue(webDriverText.getText().contains("WebDriver"));
+
+        waitAndClose();
     }
 
     @Test
     public void test2() {
+
         driver.get(" https://qatest.twoplugs.com/");
+
+        WebElement improved = driver.findElement(By.xpath("//*[@id='details-button']"));
+        improved.click();
+
+        WebElement moveOn = driver.findElement(By.xpath("//*[@id='proceed-link']"));
+        moveOn.click();
 
         WebElement element = driver.findElement(By.xpath("//span[text()='What do I need to sign up?']"));
         wait.until(ExpectedConditions.elementToBeClickable(element)).click();
@@ -38,23 +56,39 @@ public class Wait extends BaseDriver {
         WebElement text = driver.findElement(By.xpath("//p[text()='You need a facebook account or a valid email address to sign up.']"));
         wait.until(ExpectedConditions.visibilityOf(text));
 
-//        Assert.assertEquals("You need a facebook account or a valid email address to sign up.", text.getText()); // 1.yontem
-        Assert.assertTrue(text.isDisplayed());  // 2.yontem
+        // The first method
+        Assert.assertEquals("You need a facebook account or a valid email address to sign up.", text.getText());
+        // The second method
+        Assert.assertTrue(text.isDisplayed());
+
+        waitAndClose();
     }
 
     @Test
     public void test3() {
+
         driver.get("https://www.google.com/");
 
+        List<WebElement> rejectAll = driver.findElements(By.xpath("//*[@id='W0wltc']/div"));
+        if (!rejectAll.isEmpty()) {
+            rejectAll.get(0).click();
+        }
+
+        WebElement searchInput = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
+
         Actions actions = new Actions(driver);
-        actions.sendKeys(driver.findElement(By.xpath("//*[@title='Ara']")), "selenium");
-        actions.sendKeys(Keys.ENTER);
-        actions.perform();
+        Action action = actions
+                .moveToElement(searchInput)
+                .click()
+                .sendKeys("selenium"+ Keys.ENTER)
+                .build();
+        action.perform();
 
-        WebElement text = driver.findElement(By.xpath("//h3[text()='Selenium']"));
-        wait.until(ExpectedConditions.elementToBeClickable(text));
+        WebElement theFirstLink = driver.findElement(By.xpath("//h3[theFirstLink()='Selenium']"));
+        wait.until(ExpectedConditions.elementToBeClickable(theFirstLink));
 
-        Assert.assertEquals("Selenium", text.getText());
+        Assert.assertEquals("Selenium", theFirstLink.getText());
+
         waitAndClose();
     }
 }
