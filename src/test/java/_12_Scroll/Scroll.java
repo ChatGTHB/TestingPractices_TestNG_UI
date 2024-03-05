1662a8b82a6bf8dcc271bbcb28c8f1a45020d12c
ChatGTHB <cihat.kose@hotmail.com>
2024-03-05 01:23:36 +0100

    Edited

--- src/test/java/_12_Scroll/Scroll.java
+++ src/test/java/_12_Scroll/Scroll.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import utility.BaseDriver;
+import utility.MyFunction;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ public class Scroll extends BaseDriver {
 
     @Test
     public void scrollDownUp() {
+
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         WebElement user = driver.findElement(By.cssSelector("[name='username']"));
         WebElement pass = driver.findElement(By.cssSelector("[name='password']"));
@@ -29,53 +31,24 @@ public class Scroll extends BaseDriver {
 
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        MyFunction.wait(3);
         js.executeScript("window.scrollTo(document.body.scrollHeight,0)");
     }
 
     @Test
-    public void scrollElement() {
-        driver.get("https://www.lambdatest.com/");
-        WebElement ready = driver.findElement(By.xpath("//*[text()='Ready to Get Started?']"));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView();", ready);
-        System.out.println(ready.getLocation());
-    }
-    //(77, 7062)
-
-    @Test
-    public void scrollToLoc() {
-        driver.get("https://www.lambdatest.com/");
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scroll(77,7011)");
-    }
-
-    @Test
-    public void scrollHor() {
-        driver.get("https://www.album.alexflueras.ro/index.php");
-        WebElement kedicik1 = driver.findElement(By.id("a7"));
-        WebElement kedicik2 = driver.findElement(By.id("a14"));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView();", kedicik1);
-        js.executeScript("arguments[0].scrollIntoView();", kedicik2);
-    }
-
-    @Test
     public void dynamicScroll() {
+
         driver.get("https://the-internet.herokuapp.com/infinite_scroll");
         ArrayList<String> arrayList = new ArrayList<>();
         WebElement texts;
         JavascriptExecutor js = (JavascriptExecutor) driver;
 
-        int i = 1;
-        while (i < 11) {
+        for (int i = 1; i < 11; i++) {
+            MyFunction.wait(1);
             texts = driver.findElement(By.xpath("(//*[@class='jscroll-added'])[" + i + "]"));
             arrayList.add(i - 1, texts.getText());
             js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-            i++;
-        }
-
-        for (int j = 0; j < arrayList.size(); j++) {
-            System.out.println(j + 1 + ".Paragraf : " + arrayList.get(j));
+            System.out.println(i+"." + ".Paragraf : " + arrayList.get(i-1));
         }
         waitAndClose();
     }
